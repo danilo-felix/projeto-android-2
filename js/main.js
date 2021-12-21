a03ad30be0ceb8570d14b5e967167d7d25e08030
danilo-felix <danilo.felixsc@gmail.com>
2021-12-21 09:24:08 -0300

    remove transparency

--- js/main.js
+++ js/main.js
@@ -8,9 +8,11 @@ card.forEach((card) => {card.addEventListener('click', function() {
 
 
 //TRANSPARÊNCIA DA NAVBAR
-let nav = document.querySelector('.navbar');
 
-window.addEventListener('scroll', function(){
+
+/*window.addEventListener('scroll', function(){
+    let nav = document.querySelector('.navbar');
+    let button = document.querySelector('.navbar-toggler')
     if(window.pageYOffset > 1) {
         nav.classList.remove('bg-transparent')
         nav.classList.add('bg-dark');
@@ -18,4 +20,4 @@ window.addEventListener('scroll', function(){
         nav.classList.remove('bg-dark')
         nav.classList.add('bg-transparent')
     }
-})
+})*/
