05c8d9ce5176f8623d3af4fbcdd12cb89a9c42d9
danilo-felix <danilo.felixsc@gmail.com>
2021-12-21 08:34:49 -0300

    transparency navbar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-
 //CARD-FLIP
 const card = document.querySelectorAll('.card_inner');
 
 card.forEach((card) => {card.addEventListener('click', function() {
     card.classList.toggle('is-flipped');
     })
+})
+
+
+//TRANSPARÊNCIA DA NAVBAR
+let nav = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function(){
+    if(window.pageYOffset > 1) {
+        nav.classList.remove('bg-transparent')
+        nav.classList.add('bg-dark');
+    }else {
+        nav.classList.remove('bg-dark')
+        nav.classList.add('bg-transparent')
+    }
 })
